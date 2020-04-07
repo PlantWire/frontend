d75666045d65f1cb311c1418cc0834f7f2a5ced6
Oliver Göldi <oliver@goeldi.dev>
2020-04-08 01:38:18 +0200

    adapt changes of @JanHuber to bulma

--- public/js/app.js
+++ public/js/app.js
@@ -2053,6 +2053,50 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/MeasurementDisplayComponent.vue?vue&type=script&lang=js&":
+/*!**************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/MeasurementDisplayComponent.vue?vue&type=script&lang=js& ***!
+  \**************************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  mounted: function mounted() {
+    console.log('Component mounted.');
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/backo2/index.js":
 /*!**************************************!*\
   !*** ./node_modules/backo2/index.js ***!
@@ -83083,6 +83127,96 @@ render._withStripped = true
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/MeasurementDisplayComponent.vue?vue&type=template&id=63fdd39c&":
+/*!******************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/MeasurementDisplayComponent.vue?vue&type=template&id=63fdd39c& ***!
+  \******************************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", { staticClass: "card" }, [
+    _vm._m(0),
+    _vm._v(" "),
+    _c("div", { staticClass: "card-content" }, [
+      _c(
+        "div",
+        { staticClass: "content" },
+        [
+          _c("humidity-line-chart"),
+          _vm._v(" "),
+          _c("br"),
+          _vm._v("\n          Last update: "),
+          _c("time", { attrs: { datetime: "2016-1-1" } }, [
+            _vm._v("4 years ago")
+          ])
+        ],
+        1
+      )
+    ]),
+    _vm._v(" "),
+    _vm._m(1)
+  ])
+}
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("header", { staticClass: "card-header" }, [
+      _c("p", { staticClass: "card-header-title" }, [
+        _vm._v("\n          Tulip\n        ")
+      ]),
+      _vm._v(" "),
+      _c(
+        "a",
+        {
+          staticClass: "card-header-icon",
+          attrs: { href: "#", "aria-label": "hide" }
+        },
+        [
+          _c("span", { staticClass: "icon" }, [
+            _c("i", {
+              staticClass: "fas fa-angle-up",
+              attrs: { "aria-hidden": "true" }
+            })
+          ])
+        ]
+      )
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("footer", { staticClass: "card-footer" }, [
+      _c("a", { staticClass: "card-footer-item", attrs: { href: "#" } }, [
+        _vm._v("Settings")
+      ]),
+      _vm._v(" "),
+      _c("a", { staticClass: "card-footer-item", attrs: { href: "#" } }, [
+        _vm._v("Details")
+      ]),
+      _vm._v(" "),
+      _c("a", { staticClass: "card-footer-item", attrs: { href: "#" } }, [
+        _vm._v("Measure Now")
+      ])
+    ])
+  }
+]
+render._withStripped = true
+
+
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js":
 /*!********************************************************************!*\
   !*** ./node_modules/vue-loader/lib/runtime/componentNormalizer.js ***!
@@ -95329,7 +95463,8 @@ module.exports = yeast;
 
 var map = {
 	"./components/ExampleComponent.vue": "./resources/js/components/ExampleComponent.vue",
-	"./components/HumidityLineChart.vue": "./resources/js/components/HumidityLineChart.vue"
+	"./components/HumidityLineChart.vue": "./resources/js/components/HumidityLineChart.vue",
+	"./components/MeasurementDisplayComponent.vue": "./resources/js/components/MeasurementDisplayComponent.vue"
 };
 
 
@@ -95557,6 +95692,75 @@ component.options.__file = "resources/js/components/HumidityLineChart.vue"
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_HumidityLineChart_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./HumidityLineChart.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/HumidityLineChart.vue?vue&type=script&lang=js&");
 /* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_HumidityLineChart_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/MeasurementDisplayComponent.vue":
+/*!*****************************************************************!*\
+  !*** ./resources/js/components/MeasurementDisplayComponent.vue ***!
+  \*****************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _MeasurementDisplayComponent_vue_vue_type_template_id_63fdd39c___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./MeasurementDisplayComponent.vue?vue&type=template&id=63fdd39c& */ "./resources/js/components/MeasurementDisplayComponent.vue?vue&type=template&id=63fdd39c&");
+/* harmony import */ var _MeasurementDisplayComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./MeasurementDisplayComponent.vue?vue&type=script&lang=js& */ "./resources/js/components/MeasurementDisplayComponent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _MeasurementDisplayComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _MeasurementDisplayComponent_vue_vue_type_template_id_63fdd39c___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _MeasurementDisplayComponent_vue_vue_type_template_id_63fdd39c___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/MeasurementDisplayComponent.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/MeasurementDisplayComponent.vue?vue&type=script&lang=js&":
+/*!******************************************************************************************!*\
+  !*** ./resources/js/components/MeasurementDisplayComponent.vue?vue&type=script&lang=js& ***!
+  \******************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_MeasurementDisplayComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./MeasurementDisplayComponent.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/MeasurementDisplayComponent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_MeasurementDisplayComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/MeasurementDisplayComponent.vue?vue&type=template&id=63fdd39c&":
+/*!************************************************************************************************!*\
+  !*** ./resources/js/components/MeasurementDisplayComponent.vue?vue&type=template&id=63fdd39c& ***!
+  \************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_MeasurementDisplayComponent_vue_vue_type_template_id_63fdd39c___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib??vue-loader-options!./MeasurementDisplayComponent.vue?vue&type=template&id=63fdd39c& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/MeasurementDisplayComponent.vue?vue&type=template&id=63fdd39c&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_MeasurementDisplayComponent_vue_vue_type_template_id_63fdd39c___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_MeasurementDisplayComponent_vue_vue_type_template_id_63fdd39c___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
 
 /***/ }),
 
--- resources/js/components/MeasurementDisplayComponent.vue
+++ resources/js/components/MeasurementDisplayComponent.vue
@@ -1,29 +1,28 @@
 <template>
-    <div class="col-md-4">
-        <div class="card mt-3">
-
-            <div class="card-body">
-
-                <h3 class="card-title">Tulip <span class="badge badge-info">!</span></h3>
-
-
-                <h4 class="display-4">45</h4>
-
-                <p class="card-text">A tulip doesn’t strive to impress anyone. It doesn’t struggle to be different than
-                    a rose. It doesn’t have to. It is different. And there’s room in the garden for every flower. </p>
-
-                <button type="button" class="btn btn-outline-primary btn-block">Measure now!</button>
-
-                <button type="button" class="btn btn-outline-secondary btn-block">Show history</button>
-
-            </div>
-
-            <div class="card-footer">
-                Last Update: 4 Hours ago
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">
+              Tulip
+            </p>
+            <a href="#" class="card-header-icon" aria-label="hide">
+              <span class="icon">
+                <i class="fas fa-angle-up" aria-hidden="true"></i>
+              </span>
+            </a>
+        </header>
+        <div class="card-content">
+            <div class="content">
+              <humidity-line-chart></humidity-line-chart>
+              <br>
+              Last update: <time datetime="2016-1-1">4 years ago</time>
             </div>
         </div>
+        <footer class="card-footer">
+            <a href="#" class="card-footer-item">Settings</a>
+            <a href="#" class="card-footer-item">Details</a>
+            <a href="#" class="card-footer-item">Measure Now</a>
+        </footer>
     </div>
-
 </template>
 
 <script>
