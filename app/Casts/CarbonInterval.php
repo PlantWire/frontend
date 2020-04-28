339ff9667d655f3d3699f6b96a3fa763b8900545
Oliver Göldi <oliver@goeldi.dev>
2020-04-28 14:33:35 +0200

    feat(casts): add carbonInterval serializer

---
+++ app/Casts/CarbonInterval.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Carbon\Carbon;
+
+class CarbonInterval implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return CarbonInterval
+     */
+    public function get($model, $key, $value, $attributes)
+    {
+        return CarbonInterval::fromSerialized($value);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  CarbonInterval  $value
+     * @param  array  $attributes
+     * @return string
+     */
+    public function set($model, $key, $value, $attributes)
+    {
+        return $value->serialize($value);
+    }
+}
