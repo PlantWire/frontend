2fc291d22cca740a2051adda098ba31009ac6fe4
Oliver Göldi <oliver@goeldi.dev>
2020-05-03 16:41:05 +0200

    feat(scheduler): schedule request measurement job

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -13,11 +13,6 @@ class Dashboard extends Controller
 {
     public function index()
     {
-        $measurements = HumiditySensor::with('measurements')->get();
-        $measurements->each(function ($item, $key) {
-            $item->measurement_interval = CarbonInterval::hour(1);
-            $item->save();
-        });
         return view('dashboard', ['events' => Event::all(), 'sensors'=> HumiditySensor::with('measurements')->get()]);
     }
 }
