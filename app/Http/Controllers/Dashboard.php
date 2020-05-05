97043d9ec1506962fe6de55427daf95438b3187c
Jan Huber <jhuber@hsr.ch>
2020-05-05 15:18:43 +0200

    Measurements are shown in order and limited to 15

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -13,6 +13,15 @@ class Dashboard extends Controller
 {
     public function index()
     {
-        return view('dashboard', ['events' => Event::all(), 'sensors'=> HumiditySensor::with('measurements')->get()]);
+        return view('dashboard', ['events' => Event::all(), 'sensors'=>
+            HumiditySensor
+                ::with(array('measurements' => function($query) {
+                    $query
+                        ->orderBy('created_at', 'ASC')
+                    ;
+                }))
+                ->orderBy('name', 'ASC')
+                ->get()
+        ]);
     }
 }
