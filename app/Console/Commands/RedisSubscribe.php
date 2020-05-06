6e6b0883d814ef8f1798e3ca9bd033163bf633be
Oliver Göldi <oliver@goeldi.dev>
2020-05-06 08:34:12 +0200

    update demo version

--- app/Console/Commands/RedisSubscribe.php
+++ app/Console/Commands/RedisSubscribe.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Event;
+use App\HumidityMeasurement;
 
 class RedisSubscribe extends Command
 {
@@ -38,9 +39,13 @@ class RedisSubscribe extends Command
      */
     public function handle()
     {
-        \Illuminate\Support\Facades\Redis::subscribe(['pwire-frontend'], function ($message) {
+        \Illuminate\Support\Facades\Redis::subscribe(['pwire-frontend'], function ($raw) {
+            $message = json_decode($raw);
+            if($message->type == 'HumidityMeasurementResponse') {
+                $measurement = new HumidityMeasurement($message->content->value);
+            }
             $event = new Event();
-            $event->content = utf8_encode($message->content);
+            $event->content = utf8_encode($message);
             $event->save();
             Log::info('Handled event of type '.$message->type.' from '.$message->sender);
         });
