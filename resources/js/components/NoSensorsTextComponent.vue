0afde66ed931c6832e36243e2b8be8aee4774bd2
Jan Huber <jhuber@hsr.ch>
2020-05-12 08:20:53 +0200

    Add Welcome-Text for Guest-Users and Placeholder if there are no sensors

---
+++ resources/js/components/NoSensorsTextComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="column is-full">
+        <div class="column is-three-fifths is-offset-one-fifth">
+
+            <h1 class="title is-1 is-spaced has-text-centered">You have no sensors yet</h1>
+
+            <a class="button is-primary is-medium is-fullwidth" href="/create_sensor">
+                Click here to add your first sensor
+            </a>
+
+        </div>
+    </div>
+</template>
+
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,11 +6,21 @@
     <section class="section">
         <div class="container">
             <div class="columns is-multiline">
-                @foreach ($sensors as $sensor)
-                    <measurement-display-component :sensor="{{ $sensor }}">
-                    </measurement-display-component>
-                @endforeach
-                <add-sensor-card-component></add-sensor-card-component>
+                @guest
+                    <welcome-text-component></welcome-text-component>
+                @endguest
+
+                @auth
+                    @if(empty($sensors))
+                        <no-sensors-text-component></no-sensors-text-component>
+                    @else
+                        @foreach ($sensors as $sensor)
+                            <measurement-display-component :sensor="{{ $sensor }}">
+                            </measurement-display-component>
+                        @endforeach
+                        <add-sensor-card-component></add-sensor-card-component>
+                    @endif
+                @endauth
             </div>
         </div>
     </section>
