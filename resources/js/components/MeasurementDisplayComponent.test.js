967205c159649cbe3b1b173c68acb520c41c0622
Jan Huber <jhuber@hsr.ch>
2020-05-12 09:44:58 +0200

    Fix broken test

--- resources/js/components/MeasurementDisplayComponent.test.js
+++ resources/js/components/MeasurementDisplayComponent.test.js
@@ -1,5 +1,5 @@
 import { MeasurementHelper } from '../measurementHelper';
 
 test('generate time ago string', () => {
-    expect(MeasurementHelper.generateTimeAgoString(new Date())).toBe("1 hours ago");
+    expect(MeasurementHelper.generateTimeAgoString(new Date())).toBe("0 hours ago");
 });
