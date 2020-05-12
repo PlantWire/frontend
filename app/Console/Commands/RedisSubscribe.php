f32a13fdacbcbfaad4e96fd12931f095d9be373f
Oliver Göldi <oliver@goeldi.dev>
2020-05-13 00:49:29 +0200

    small fixes

--- app/Console/Commands/RedisSubscribe.php
+++ app/Console/Commands/RedisSubscribe.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Event;
 use App\HumidityMeasurement;
+use App\HumiditySensor;
 
 class RedisSubscribe extends Command
 {
@@ -43,9 +44,12 @@ class RedisSubscribe extends Command
             $message = json_decode($raw);
             if($message->type == 'HumidityMeasurementResponse') {
                 $measurement = new HumidityMeasurement($message->content->value);
+                $measurement->sensor = HumiditySensor::where('uuid', $message->sender);
+                $measurement->save();
             }
             $event = new Event();
-            $event->content = utf8_encode($message);
+            $event->content = utf8_encode($raw);
+            $event->sensor = HumiditySensor::where('uuid', $message->sender);
             $event->save();
             Log::info('Handled event of type '.$message->type.' from '.$message->sender);
         });
