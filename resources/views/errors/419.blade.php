adb67f8ccfa580a195de2c9cf7941bd4d0850285
SHafner <shafner@hsr.ch>
2020-05-18 12:53:02 +0200

    error pages with 'return to dashboard' button

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.errors')
+@section('title', __('Not Found'))
+@section('content')
+<div class="flex-center position-ref full-height">
+    <div class="column is-full">
+        <div class="field">
+            <div class="code is-inline">419</div>
+            <div class="message is-inline">{{ __('Page Expired') }}</div>
+        </div>
+        <div class="field">
+            <a href="/" class="button">{{ __('control-elements.return_to_dashboard') }}</a>&nbsp;
+        </div>
+    </div>
+</div>
+@endsection
