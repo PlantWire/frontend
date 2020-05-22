3d695848291f07fd1d7b4bb2eb75ea3521a56ee2
Oliver Göldi <oliver@goeldi.dev>
2020-05-22 12:34:28 +0200

    use fontawesome via npm import to save space and midigate update issues

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
-        <script defer src="{{ mix('/fontawesome/js/all.js') }}"></script>
         <title>pWire - @yield('title')</title>
     </head>
     <body>
